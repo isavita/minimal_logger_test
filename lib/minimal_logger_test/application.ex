ff6725e54d514a48de310025a1cebfb379178387
Aleksandar Dimov <aleksander.dimov@bbc.co.uk>
2025-10-09 08:39:11 +0100

    Add filtering with solution

--- lib/minimal_logger_test/application.ex
+++ lib/minimal_logger_test/application.ex
@@ -3,20 +3,18 @@ defmodule MinimalLoggerTest.Application do
 
   @impl true
   def start(_type, _args) do
-    # Add file handler for logging
+    # Remove the default handler to avoid formatter conflicts
+    :logger.remove_handler(:default)
+
+    # Add file handler with JSON formatter and metadata filtering
     :logger.add_handler(:file_log, :logger_std_h, %{
-      config: %{
-        file: ~c"/tmp/bandit_test.log",
-        filesync_repeat_interval: 5000,
-        file_check: 5000,
-        max_no_bytes: 10_000_000,
-        max_no_files: 5
-      },
-      formatter: {:logger_formatter, %{
-        template: [:msg, "\n"]
-      }},
+      config: %{file: ~c"/tmp/bandit_test.log"},
       level: :error,
-      filters: []
+      formatter: {MinimalLoggerTest.Formatter, %{}},
+      filter_default: :log,
+      filters: [
+        {:remove_crash_reason, {&filter_crash_reason/2, []}}
+      ]
     })
 
     children = [
@@ -24,13 +22,16 @@ defmodule MinimalLoggerTest.Application do
        plug: MinimalLoggerTest.TimeoutPlug,
        scheme: :http,
        port: 4000,
-       thousand_island_options: [
-         read_timeout: 20
-       ],
+       thousand_island_options: [read_timeout: 20],
        http_options: [compress: false]}
     ]
 
     opts = [strategy: :one_for_one, name: MinimalLoggerTest.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp filter_crash_reason(log_event, _extra) do
+    %{meta: meta} = log_event
+    %{log_event | meta: Map.delete(meta, :crash_reason)}
   end
 end
--- lib/minimal_logger_test/formatter.ex
+++ lib/minimal_logger_test/formatter.ex
@@ -1,13 +1,19 @@
 defmodule MinimalLoggerTest.Formatter do
-  def format(level, message, _timestamp, metadata) do
+  def format(log_event, _config) do
+    %{level: level, msg: msg, meta: meta} = log_event
+
     Jason.encode!(%{
       level: level,
-      message: to_string(message),
-      metadata: stringify_metadata(metadata)
+      message: format_msg(msg),
+      metadata: stringify_metadata(meta)
     }) <> "\n"
   end
 
-  defp stringify_metadata(metadata) do
-    Map.new(metadata, fn {k, v} -> {k, inspect(v)} end)
+  defp format_msg({:string, chardata}), do: IO.chardata_to_string(chardata)
+  defp format_msg({:report, report}), do: inspect(report)
+  defp format_msg(msg), do: to_string(msg)
+
+  defp stringify_metadata(meta) do
+    Map.new(meta, fn {k, v} -> {k, inspect(v)} end)
   end
 end
